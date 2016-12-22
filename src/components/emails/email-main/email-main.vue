c1275a14df96a73c314ff6fd7402ddee305a5055
Avital <avitalbarzelai@gmail.com>
2016-12-22 18:10:48 +0200

    email-compse component set up

--- src/components/emails/email-main/email-main.vue
+++ src/components/emails/email-main/email-main.vue
@@ -1,7 +1,6 @@
-  <template src="./emails.component.html">
-
-  </template>
-  
+  <template src="./emails.component.html"></template>
+<script src="./emails.component.js"></script>
+<style src="./emails.component.scss" scoped lang="scss"></style>
   
   
   
--- src/main.js
+++ src/main.js
@@ -1,31 +1,34 @@
 import style from './style.scss';
-import Vue from 'vue'
+import Vue from 'vue';
+import VueResource from 'vue-resource';
 import VueRouter from 'vue-router';
+// import App from './App.vue'
 
 import Home from './components/home';
 import About from './components/about';
-import Emails from './components/emails/email-main';
+import Emails from './components/emails/email-main/email-main';
 import MainNav from './components/main-nav';
 import Events from './components/events';
 
+Vue.use(VueResource);
 Vue.use(VueRouter);
+Vue.http.options.root = 'http://localhost:3003';
 
 const routes = [{
-  path: '/',
-  component: Home
-}, {
-  path: '/about',
-  component: About
+    path: '/',
+    component: Home
+  }, {
+    path: '/about',
+    component: About
 
-},{
-  path: '/events',
-  component: Events
-},{
-  path: '/emails',
-  component: Emails
-}
+  },{
+    path: '/events',
+    component: Events
+  },{
+    path: '/emails',
+    component: Emails
+  }
 ];
-
 
 
 const router = new VueRouter({
