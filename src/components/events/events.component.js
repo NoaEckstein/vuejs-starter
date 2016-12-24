0541647be3e7738568422a12293991a9ef408036
Noa <nonistein@gmail.com>
2016-12-24 19:37:31 +0200

    linking events in server to DOM project

--- src/components/events/events.component.js
+++ src/components/events/events.component.js
@@ -1,8 +1,20 @@
-export default  {
+export default {
   data: () => {
     return {
-        
+      events: []
+    }
+  },
+  methods: {
+    reloadEvents() {
+      this.$http.get('event')
+        .then(res => res.json())
+        .then(events => this.events = events);
+      // this.carToEdit = undefined;
+      // this.showCarEdit = false;
+    }
+  },
+     created() {
+           this.reloadEvents();
+            
        }
-      },
-     }
-  
+}
--- src/components/home/home.component.js
+++ src/components/home/home.component.js
@@ -5,21 +5,5 @@ export default {
     }
   },
   methods: {
-
-    reloadCars() {
-      // fetch('http://localhost:3003/item')
-      this.$http.get('item')
-        .then(res => res.json())
-        .then(cars => this.cars = cars);
-      this.carToEdit = undefined;
-      this.showCarEdit = false;
-      console.log('hi!');
-      
-
-    }
   },
-  created() {
-    this.reloadCars();
-
-  }
 }
