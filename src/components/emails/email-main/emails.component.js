60e213a0593e5304b98c4b8e65bbe65accfdc5a7
orharpaz <orharpaz1@gmail.com>
2016-12-25 11:26:14 +0200

    c

--- src/components/emails/email-main/emails.component.js
+++ src/components/emails/email-main/emails.component.js
@@ -1,42 +1,22 @@
-<<<<<<< HEAD
-import emailPrev from '../email-preview/email-preview.vue'
+// import emailPrev from '../email-preview/email-preview.vue'
 import emailList from '../email-list/email-list.vue'
-
-
-
-=======
- 
 import emailcompose from '../email-compose/email-compose.vue';
->>>>>>> ee1c89836db56388ab5f5cfa31f2b5c31d4193f5
-export default  {
+
+export default {
 
 
   data: () => {
 
-    
     return {
-<<<<<<< HEAD
-         emails: [
-                {name: 'wowmotherfuckaaa', id: 1}
-                ]
-               
-      
+      name: 'avital'
     }
   },
 
   components: {
-            'email-preview' : emailPrev,
-            'email-list' : emailList
-
-        }
-  
-=======
-      name: 'avital'
-      
-    }
-  },
-  components:{
+    'email-preview': emailPrev,
+    'email-list': emailList,
     emailcompose
+
   }
->>>>>>> ee1c89836db56388ab5f5cfa31f2b5c31d4193f5
 }
+  
